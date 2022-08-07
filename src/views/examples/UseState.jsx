c54714cd9bcdb6fb878de0772caa17c99aff3425
Gustavo Kazuo <kazucalado@gmail.com>
2022-08-07 15:37:18 -0300

    useState part one :first:

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle.jsx";
 
 const UseState = (props) => {
-    return (
-        <div className="UseState">
-            <PageTitle
-                title="Hook UseState"
-                subtitle="Estado em componentes funcionais!"
-            />
-        </div>
-    )
-}
+  const [count, setCount] = useState(0);
 
-export default UseState
+  return (
+    <div className="UseState">
+      <PageTitle
+        title="Hook UseState"
+        subtitle="Estado em componentes funcionais!"
+      />
+      <SectionTitle title="Exercise 1" />
+      <div className="center">
+        <span className="text">{count}</span>
+        <div>
+          <button className="btn" onClick={() => setCount(count - 1)}>
+            -1
+          </button>
+          <button className="btn" onClick={() => setCount(count + 1)}>
+            +1
+          </button>
+          <button
+            className="btn"
+            onClick={() => setCount((cuttect) => cuttect + 1000)}
+          >
+            +1000
+          </button>
+        </div>
+      </div>
+      <SectionTitle title="Exercise 2" />
+    </div>
+  );
+};
+
+export default UseState;
