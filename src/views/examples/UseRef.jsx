4ce0cc41c11ea4ce1c538e788318b89abd90259c
Gustavo Kazuo <kazucalado@gmail.com>
2022-08-13 05:27:03 -0300

    :sparkles: create UseRef

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,15 +1,43 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useEffect, useRef, useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 const UseRef = (props) => {
-    return (
-        <div className="UseRef">
-            <PageTitle
-                title="Hook UseRef"
-                subtitle="Retorna um objeto mutável com a propriedade .current!"
-            />
-        </div>
-    )
-}
+  // mod estado componente renderiza utomatico
+  const [value1, setValue1] = useState("");
+  //nao renderiza automaticamente
+  const count = useRef(0);
 
-export default UseRef
+  useEffect(() => {
+    count.current = count.current + 1;
+  }, [value1]);
+  //roda logo antes do componente ser renderizado
+  //   count.current = count.current + 1;
+  return (
+    <div className="UseRef">
+      <PageTitle
+        title="Hook UseRef"
+        subtitle="Retorna um objeto mutável com a propriedade .current!"
+      />
+      <SectionTitle title="Exercicio 1" />
+      <div className="center">
+        <div>
+          <span className="text">Valor:</span>
+          <span className="text">{value1}[</span>
+
+          <span className="text red">{count.current}</span>
+          <span className="text">]</span>
+        </div>
+        <input
+          type="text"
+          className="input"
+          value={value1}
+          onChange={(e) => setValue1(e.target.value)}
+        />
+        {console.log((e) => e.target)}
+      </div>
+    </div>
+  );
+};
+
+export default UseRef;
