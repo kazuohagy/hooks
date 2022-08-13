57d322ec51fbb3e8682dc9857a3decab8ced78f6
Gustavo Kazuo <kazucalado@gmail.com>
2022-08-13 06:04:01 -0300

    :bug: ?

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -2,15 +2,27 @@ import React, { useEffect, useRef, useState } from "react";
 import PageTitle from "../../components/layout/PageTitle";
 import SectionTitle from "../../components/layout/SectionTitle";
 
+const merge = function () {};
+
 const UseRef = (props) => {
   // mod estado componente renderiza utomatico
   const [value1, setValue1] = useState("");
+  const [value2, setValue2] = useState("");
   //nao renderiza automaticamente
   const count = useRef(0);
+  const inputRef1 = useRef(null);
+  const inputRef2 = useRef(null);
+
+  console.log(inputRef1.current);
 
   useEffect(() => {
     count.current = count.current + 1;
+    inputRef2.current.focus();
   }, [value1]);
+  useEffect(() => {
+    count.current++;
+    inputRef1.current.focus();
+  }, [value2]);
   //roda logo antes do componente ser renderizado
   //   count.current = count.current + 1;
   return (
@@ -32,9 +44,19 @@ const UseRef = (props) => {
           type="text"
           className="input"
           value={value1}
+          ref={inputRef1}
           onChange={(e) => setValue1(e.target.value)}
         />
-        {console.log((e) => e.target)}
+      </div>
+      <SectionTitle title="Exercicio 2" />
+      <div className="center">
+        <input
+          type="text"
+          className="input"
+          ref={inputRef2}
+          value={value2}
+          onChange={(e) => setValue2(e.target.value)}
+        />
       </div>
     </div>
   );
