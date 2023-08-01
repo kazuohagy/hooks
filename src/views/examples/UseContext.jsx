2d6f4164d4d282aadf66b41007251babaef4bb50
kazuohagy <kazuohagy@users.noreply.github.com>
2023-08-01 09:55:04 -0300

    useContext

--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -4,6 +4,12 @@ import DataContext from '../../data/DataContext'
 const UseContext = (props) => {
     const context = useContext(DataContext)
 
+    function setNumber(n){
+        context.setState({
+            ...context.state,
+            number: context.state.number + n
+        })
+    }    
     return (
         <div className="UseContext">
             <PageTitle
@@ -11,8 +17,18 @@ const UseContext = (props) => {
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
             <div className="center">
-                <span className="text">{context.text}</span>
-                <span className="text">{context.number}</span>
+                <span className="text">{context.state.text}</span>
+                <span className="text">{context.state.number}</span>
+            </div>
+            <div className="center">
+                <div className="">
+                    <button className="btn" onClick={()=> setNumber(1)}>
+                        +1
+                    </button>                
+                    <button className="btn" onClick={()=> setNumber(-1)}>
+                        -1
+                    </button>
+                </div>
             </div>
         </div>
     )
