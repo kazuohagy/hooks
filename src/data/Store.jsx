4a4105ea5cfb29d232e3acfc783181d40928b26c
kazuohagy <kazuohagy@users.noreply.github.com>
2023-08-02 23:55:26 -0300

    jesus

---
+++ src/data/Store.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const initialSate = {
+    number:1234, 
+    text: 'Context API + HOOKS MARIO'
+}
+const AppContext = React.createContext(initialSate)
+const Store =  props => {
+    const [state, setState] = React.useState(initialSate)
+    function updateState(key, value){
+        setState({
+            ...state,
+            [key]: value
+        })
+    }
+    return(
+        <AppContext.Provider value={{
+            number: state.number,
+            text: state.text,
+            setNumber: n => updateState('number', n),
+            setText: t => updateState('text', t)
+            }}>
+            {props.children}
+        </AppContext.Provider>
+    )
+    }
+
+export default Store;
