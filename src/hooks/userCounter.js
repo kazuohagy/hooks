641dbbe61b22951f8237b65816cf52803001656d
kazuohagy <kazuohagy@users.noreply.github.com>
2023-08-09 23:43:01 -0300

    deu certo

---
+++ src/hooks/userCounter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+export const useCounter = (initialValue = 100) => {
+    const [count, setCount] = React.useState(initialValue)
+    function inc() { 
+        setCount(count + 1)
+    }
+    function dec() {
+        setCount(count - 1)
+    }
+    return [count, inc, dec]
+}
--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+import { useCounter } from '../../hooks/userCounter' // Corrigido o caminho para useCounter
 
 const UseRef = (props) => {
+    const [count, inc, dec] = useCounter()
     return (
         <div className="UseCustom">
             <PageTitle
                 title="Seu Hook"
                 subtitle="Vamos aprender como criar o nosso próprio Hook!"
             />
+            <SectionTitle title="Exercício #01"/>
+            <div className="center">
+                <span className="text">{count}</span> {/* Exibe o valor do contador */}
+                <div className="button-container"> {/* Adiciona classes para o div dos botões */}
+                    <button className="btn" onClick={() => inc()}>+</button>
+                    <button className="btn" onClick={() => dec()}>-</button>
+                </div>
+            </div>
         </div>
     )
 }
